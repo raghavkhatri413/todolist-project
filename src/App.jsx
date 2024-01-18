d05401efffedaf256bcb497369cd2cab213f1c98
Raghav Khatri <khatriraghav14@gmail.com>
2024-01-18 22:38:03 +0530

    ToDoList-v1

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { NewTodoForm } from "./NewTodoForm";
+import "./styles.css";
+import { TodoList } from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState(()=>{
+    const localValue=localStorage.getItem("ITEMS")
+    if(localValue==null)  return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(()=>{
+    localStorage.setItem("ITEMS",JSON.stringify(todos))
+  },[todos])
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return <div>
+    <NewTodoForm onSubmit={addTodo} />
+    <h1 className="header">To Do List</h1>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+  </div>
+}
